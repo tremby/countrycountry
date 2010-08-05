1a755f9dfd1b8d3eafabfd96e9ad4f5afa8dfc6e
Bart Nagel <bart@tremby.net>
2010-08-05 13:27:49 +0100

    added link to viewmanycollectionresults to mainmenu

--- content/mainmenu.php
+++ content/mainmenu.php
@@ -3,6 +3,7 @@
 <ul>
 	<li><a href="<?php echo SITEROOT_WEB; ?>newcollection">New collection</a></li>
 	<li><a href="<?php echo SITEROOT_WEB; ?>existingcollections">Existing collections</a></li>
-	<li><a href="<?php echo SITEROOT_WEB; ?>viewcollectionresults">View collection results</a></li>
+	<li><a href="<?php echo SITEROOT_WEB; ?>viewcollectionresults">View results from a single collection</a></li>
+	<li><a href="<?php echo SITEROOT_WEB; ?>viewmanycollectionresults">Compare results from multiple collections</a></li>
 </ul>
 <?php include "htmlfooter.php"; ?>
