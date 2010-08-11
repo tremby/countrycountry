099bf2f3ae2a3a24942e06bd8b2090580deccf78
Bart Nagel <bart@tremby.net>
2010-08-11 15:07:25 +0100

    added links to ECS, OeRC, DOC

--- include/htmlfooter.php
+++ include/htmlfooter.php
@@ -1,6 +1,12 @@
 </div>
 <div id="footer">
 	<a href="http://www.nema.ecs.soton.ac.uk">NEMA project</a>
+	<br>
+	A collaboration between
+	<a href="http://www.ecs.soton.ac.uk">the School of Electronics and Computer Science, University of Southampton</a>,
+	<a href="http://www.oerc.ox.ac.uk">Oxford e-Research Centre, University of Oxford</a>
+	and
+	<a href="http://www.gold.ac.uk/computing">Department of Computing, Goldsmiths University of London</a>
 </div>
 </body>
 </html>
