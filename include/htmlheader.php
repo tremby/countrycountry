18aecfa05330d74494b39ec6877a0e7a4b82a03a
Bart Nagel <bart@tremby.net>
2010-09-16 14:56:23 +0100

    added types to stylesheet links

--- include/htmlheader.php
+++ include/htmlheader.php
@@ -15,15 +15,17 @@ header("Content-Script-Type: text/javascript");
 	<script type="text/javascript" src="<?php echo SITEROOT_WEB; ?>include/flot/jquery.flot.js"></script>
 	<script type="text/javascript" src="<?php echo SITEROOT_WEB; ?>include/flot/jquery.flot.pie.js"></script>
 	<script type="text/javascript" src="<?php echo SITEROOT_WEB; ?>include/flot/jquery.flot.orderBars.js"></script>
+
 	<script type="text/javascript" src="<?php echo SITEROOT_WEB; ?>include/jquery.jplayer/jquery.jplayer.js"></script>
-	<link rel="stylesheet" href="<?php echo SITEROOT_WEB; ?>include/jquery.jplayer/jplayer.blue.monday.css">
+	<link rel="stylesheet" href="<?php echo SITEROOT_WEB; ?>include/jquery.jplayer/jplayer.blue.monday.css" type="text/css">
+
 	<script type="text/javascript" src="<?php echo SITEROOT_WEB; ?>include/collectionbuilder.js"></script>
 	<?php if (isset($GLOBALS["headerjs"])) { ?>
 		<script type="text/javascript">
 			<?php echo $GLOBALS["headerjs"]; ?>
 		</script>
 	<?php } ?>
-	<link rel="stylesheet" href="<?php echo SITEROOT_WEB; ?>include/styles.css">
+	<link rel="stylesheet" href="<?php echo SITEROOT_WEB; ?>include/styles.css" type="text/css">
 	<?php if (isset($GLOBALS["headercss"])) { ?>
 		<style type="text/css">
 			<?php echo $GLOBALS["headercss"]; ?>
