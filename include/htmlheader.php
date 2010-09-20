e2a96b101a2b0c6b7618f95202abfacd74bd2efd
Bart Nagel <bart@tremby.net>
2010-09-20 17:18:27 +0100

    added google analytics

--- include/htmlheader.php
+++ include/htmlheader.php
@@ -37,6 +37,19 @@ header("Content-Script-Type: text/javascript");
 			<?php echo $GLOBALS["headercss"]; ?>
 		</style>
 	<?php } ?>
+	<script type="text/javascript">
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-18639058-1']);
+		_gaq.push(['_trackPageview']);
+		(function() {
+			var ga = document.createElement('script');
+			ga.type = 'text/javascript';
+			ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0];
+			s.parentNode.insertBefore(ga, s);
+		})();
+	</script>
 </head>
 <body>
 <div id="header">
