b79240a319fe6f0b09f43edb15926bcaf326dafb
Bart Nagel <bart@tremby.net>
2010-09-22 19:28:20 +0100

    can't delete bjn's collections for demo purposes

--- content/deletecollection.php
+++ content/deletecollection.php
@@ -3,6 +3,12 @@
 if (!isset($_REQUEST["id"]))
 	badrequest("No collection ID specified");
 
+$errors = array();
+$collection = getcollectioninfo("http://collections.nema.ecs.soton.ac.uk/signalcollection/" . $_REQUEST["id"], $errors);
+$aggregate = $collection["index"][$collection["uri"] . "#aggregate"];
+if ($aggregate[$ns["dc"] . "creator"][0] == "bjn")
+	badrequest("For demo purposes you can't delete collections by bjn.");
+
 $filename = SITEROOT_LOCAL . "signalcollections/" . $_REQUEST["id"] . ".xml";
 if (file_exists($filename) && dirname($filename) == SITEROOT_LOCAL . "signalcollections") {
 	unlink($filename);
