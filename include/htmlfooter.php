889abe4a542052ab9e5f5c410328c2b605e48e33
Bart Nagel <bart@tremby.net>
2010-10-13 17:36:51 +0100

    added Imirsel credit

--- include/htmlfooter.php
+++ include/htmlfooter.php
@@ -24,13 +24,15 @@
 	</div>
 </div>
 <div id="footer">
-	<a href="http://www.nema.ecs.soton.ac.uk">NEMA project</a>
-	<br>
-	A collaboration between
-	<a href="http://www.ecs.soton.ac.uk">the School of Electronics and Computer Science, University of Southampton</a>,
-	<a href="http://www.oerc.ox.ac.uk">Oxford e-Research Centre, University of Oxford</a>
-	and
-	<a href="http://www.gold.ac.uk/computing">Department of Computing, Goldsmiths University of London</a>
+	<p><a href="http://www.nema.ecs.soton.ac.uk">NEMA project</a></p>
+	<p>
+		A collaboration between
+		<a href="http://www.ecs.soton.ac.uk">the School of Electronics and Computer Science, University of Southampton</a>,
+		<a href="http://www.oerc.ox.ac.uk">Oxford e-Research Centre, University of Oxford</a>
+		and
+		<a href="http://www.gold.ac.uk/computing">Department of Computing, Goldsmiths University of London</a>
+	</p>
+	<p>Many thanks to Stephen Downie and his team at <a href="http://www.music-ir.org/">IMIRSEL</a>, University of Illinois, for access and source code to their "Son of Blinkie" genre classification workflow.</a>
 </div>
 </body>
 </html>
