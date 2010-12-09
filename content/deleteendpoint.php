bd82fdaf4e0d97e6e49e88e07686aeddd7939e67
Bart Nagel <bart@tremby.net>
2010-12-09 15:52:27 +0000

    delete endpoint now changes its behaviour based on Accept header

--- content/deleteendpoint.php
+++ content/deleteendpoint.php
@@ -3,10 +3,16 @@
 if (!isset($_POST["id"]))
 	badrequest("No endpoint ID specified");
 
-$errors = array();
 $endpoint = Endpoint::load($_POST["id"], true) or badrequest("No endpoint with the given ID exists");
-$endpoint->delete() or servererror("failed to delete endpoint '" . $endpoint->url() . "'");
+$success = $endpoint->delete();
 
-ok(json_encode(array("id" => $_POST["id"])), "application/json");
+if (preferredtype(array("application/json", "text/html")) == "application/json") {
+	if ($success)
+		ok(json_encode(array("id" => $_POST["id"])), "application/json");
+	servererror("failed to delete endpoint '" . $endpoint->name() . "'");
+}
+
+flash("Endpoint '" . $endpoint->name() . "' successfully deleted");
+redirect("endpointadmin");
 
 ?>
